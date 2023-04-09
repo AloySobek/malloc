a868a2df71c50246947186dca06310fd0c15bffc
AloySobek <supernova.wandering.star@gmail.com>
2023-04-09 19:43:53 +0300

    chore: Simplified project structure

--- src/main.c
+++ src/main.c
@@ -1,15 +1,3 @@
-/**
- * File              : main.c
- * Author            : Rustam Khafizov <super.rustamm@gmail.com>
- * Date              : 21.05.2021 16:30
- * Last Modified Date: 21.05.2021 16:31
- * Last Modified By  : Rustam Khafizov <super.rustamm@gmail.com>
- */
-
 #include "malloc.h"
 
-int main(int argc, char **argv)
-{
-
-    return (0);
-}
+int main(int argc, char **argv) { return (0); }
--- src/realloc.c
+++ src/realloc.c
@@ -1,16 +1,7 @@
-/**
- * File              : realloc.c
- * Author            : Rustam Khafizov <super.rustamm@gmail.com>
- * Date              : 21.05.2021 16:33
- * Last Modified Date: 21.05.2021 17:01
- * Last Modified By  : Rustam Khafizov <super.rustamm@gmail.com>
- */
-
 #include "malloc.h"
 
-void *malloc(size_t size)
-{
-    if (size == 0) 
-        return (NULL);
+void *realloc(void *ptr, size_t size) {
+  if (size == 0)
     return (NULL);
+  return (NULL);
 }
