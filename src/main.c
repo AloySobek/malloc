94de82290fa7f97ef973d2b6d7f3ba7bc97fe0d0
AloySobek <supernova.wandering.star@gmail.com>
2023-12-03 23:24:12 +0100

    feat: Simplest possible malloc implementation, lol

--- src/main.c
+++ src/main.c
@@ -4,40 +4,44 @@
 #include <sys/mman.h>
 #include <unistd.h>
 
-#define N 5
+#define TINY_N 128
+#define TINY_SIZE 32
 
-typedef struct __s_malloc_state {
-    int first_call; // Lazy loading bit, dirty bit, etc...
-} __t_malloc_state;
+#define SMALL_N 128
+#define SMALL_SIZE 64
 
-__t_malloc_state __malloc_state;
+#define LARGE_N 128
+#define LARGE_SIZE 256
 
-size_t global_variable = 0;
-
-void prealloc() {}
+struct meta {
+    size_t size;
+};
 
 void *mymalloc(size_t size) {
-    void *ptr;
-
-    ptr = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, 0, 0);
+    void *ptr = mmap(NULL, sizeof(struct meta) + size, PROT_READ | PROT_WRITE,
+                     MAP_PRIVATE | MAP_ANONYMOUS, 0, 0);
 
     if (ptr == MAP_FAILED) {
-        printf("Mapping Failed\n");
-
-        return (NULL);
+        return NULL;
     }
 
-    global_variable = size;
+    (*((struct meta *)ptr)).size = size;
 
-    return ptr;
+    return ptr + sizeof(struct meta);
 }
 
 void myfree(void *ptr) {
-    int err = munmap(ptr, global_variable);
-
-    if (err != 0) {
-        printf("UnMapping Failed\n");
+    if (ptr == NULL) {
+        return;
     }
+
+    munmap(ptr, (*((struct meta *)(ptr - sizeof(struct meta)))).size);
+}
+
+void *myrealloc(void *ptr, size_t size) {
+    myfree(ptr);
+
+    return mymalloc(size);
 }
 
 int test() {
@@ -53,38 +57,11 @@ int test() {
         printf("%d ", array[i]);
     }
 
+    printf("\n");
+
     myfree(array);
 
-    // if (ptr == MAP_FAILED) {
-    //     printf("Mapping Failed\n");
-
-    //     return 1;
-    // }
-
-    // int size = getpagesize() - 100;
-
-    // // Fill the elements of the array
-    // for (int i = 0; i < size; i++) {
-    //     ptr[i] = i * 10;
-    // }
-
-    // // Print the elements of the array
-    // printf("The elements of the array => ");
-
-    // for (int i = 0; i < N; i++)
-    //     printf("[%d] ", ptr[i]);
-
-    // printf("\n");
-
-    // int err = munmap(ptr, 10 * sizeof(int));
-
-    // if (err != 0) {
-    //     printf("UnMapping Failed\n");
-
-    //     return 1;
-    // }
-    //
-    return (0);
+    return 0;
 }
 
 int main(int argc, char **argv) {
