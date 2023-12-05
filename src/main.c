f8fc395c5ce0390619ffb383d34fb6403413248c
AloySobek <supernova.wandering.star@gmail.com>
2023-12-05 22:53:41 +0100

    feat: Simple comparison to standard malloc to treck improvments

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/mman.h>
+#include <time.h>
 #include <unistd.h>
 
 #define TINY_N 128
@@ -44,28 +45,51 @@ void *myrealloc(void *ptr, size_t size) {
     return mymalloc(size);
 }
 
-int test() {
-    int *array = NULL;
+void benchmark(int times, int bytes) {
+    clock_t start = clock();
 
-    array = mymalloc(sizeof(int) * 100);
+    for (int i = 0; i < times; ++i) {
+        char *array = mymalloc(sizeof(char) * bytes);
 
-    for (int i = 0; i < 100; ++i) {
-        array[i] = i;
+        myfree(array);
     }
 
-    for (int i = 0; i < 100; ++i) {
-        printf("%d ", array[i]);
+    clock_t end = clock();
+
+    double mydiff = difftime(end, start) / CLOCKS_PER_SEC;
+
+    printf("My malloc took %.8lf seconds to allocate and deallocate %d bytes %d times\n", mydiff,
+           bytes, times);
+
+    start = clock();
+
+    for (int i = 0; i < times; ++i) {
+        char *array = malloc(sizeof(char) * bytes);
+
+        free(array);
     }
 
-    printf("\n");
+    end = clock();
 
-    myfree(array);
+    double diff = difftime(end, start) / CLOCKS_PER_SEC;
 
-    return 0;
+    printf("Standard malloc took %.8lf seconds to allocate and deallocate %d bytes %d times\n",
+           diff, bytes, times);
+
+    printf("My malloc is %.8lf times slower than standard malloc\n", mydiff / diff);
 }
 
 int main(int argc, char **argv) {
-    assert(test() == 0);
+    if (argc != 3) {
+        printf("usage: malloc times bytes\n");
+
+        return 0;
+    }
+
+    int times = atoi(argv[1]);
+    int bytes = atoi(argv[2]);
+
+    benchmark(times, bytes);
 
     return (0);
 }
