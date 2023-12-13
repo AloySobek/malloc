e47c4ef7a6d1d49edf2bb790f4b343adc5a20445
AloySobek <supernova.wandering.star@gmail.com>
2023-12-13 15:41:16 +0100

    wip

--- src/malloc.c
+++ src/malloc.c
@@ -9,37 +9,37 @@ void *malloc(size_t size) {
         _heap = _allocate_heap(TINY_N, TINY_SIZE, SMALL_N, SMALL_SIZE);
     }
 
-    void *iter = _heap;
+    // void *iter = _heap;
 
-    for (int i = 0; i < MAX_HEAPS; ++i) {
-        void *block = NULL;
+    // for (int i = 0; i < MAX_HEAPS; ++i) {
+    void *block = NULL;
 
-        if (size <= TINY_SIZE) {
-            ((struct heap *)iter)->tiny =
-                _remove_block(((struct heap *)iter)->tiny, (struct block **)&block);
-        } else if (size <= SMALL_SIZE) {
-            ((struct heap *)iter)->small =
-                _remove_block(((struct heap *)iter)->small, (struct block **)&block);
-        } else {
-            break;
-        }
-
-        if (block != NULL) {
-            return block + sizeof(struct block);
-        }
-
-        if (((struct heap *)iter)->next == NULL) {
-            struct heap *heap = _allocate_heap(TINY_N, TINY_SIZE, SMALL_N, SMALL_SIZE);
-
-            ((struct heap *)iter)->next = heap;
-            heap->prev = iter;
-        }
-
-        iter = ((struct heap *)iter)->next;
+    if (size <= TINY_SIZE) {
+        ((struct heap *)_heap)->tiny =
+            _remove_block(((struct heap *)_heap)->tiny, (struct block **)&block);
+    } else if (size <= SMALL_SIZE) {
+        ((struct heap *)_heap)->small =
+            _remove_block(((struct heap *)_heap)->small, (struct block **)&block);
+    } else {
+        // break;
     }
 
-    void *block = mmap(NULL, sizeof(struct block) + size, PROT_READ | PROT_WRITE,
-                       MAP_PRIVATE | MAP_ANONYMOUS, 0, 0);
+    if (block != NULL) {
+        return block + sizeof(struct block);
+    }
+
+    //     if (((struct heap *)iter)->next == NULL) {
+    //         struct heap *heap = _allocate_heap(TINY_N, TINY_SIZE, SMALL_N, SMALL_SIZE);
+
+    //         ((struct heap *)iter)->next = heap;
+    //         heap->prev = iter;
+    //     }
+
+    //     iter = ((struct heap *)iter)->next;
+    // }
+
+    block = mmap(NULL, sizeof(struct block) + size, PROT_READ | PROT_WRITE,
+                 MAP_PRIVATE | MAP_ANONYMOUS, 0, 0);
 
     if (block == MAP_FAILED) {
         return NULL;
